5a0b3c76d8a33a80e94d35caa7a0ce471c0c7660
David N <bigair_1080@hotmail.com>
2018-12-31 19:00:57 -0400

    Updates

---
+++ precache-manifest.6d7538d4e0065184205816544c6f0aec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "be6e466a9b8ce775e772",
+    "url": "/Confidenceiskey/robofriends/static/js/runtime~main.be6e466a.js"
+  },
+  {
+    "revision": "4fb55c845c9e5b1cd4f4",
+    "url": "/Confidenceiskey/robofriends/static/js/main.4fb55c84.chunk.js"
+  },
+  {
+    "revision": "916f22f5d7741dc1cba8",
+    "url": "/Confidenceiskey/robofriends/static/js/1.916f22f5.chunk.js"
+  },
+  {
+    "revision": "4fb55c845c9e5b1cd4f4",
+    "url": "/Confidenceiskey/robofriends/static/css/main.e6871b24.chunk.css"
+  },
+  {
+    "revision": "916f22f5d7741dc1cba8",
+    "url": "/Confidenceiskey/robofriends/static/css/1.47659e52.chunk.css"
+  },
+  {
+    "revision": "7cadb2d9d4dd6a1759668304ac55fe9e",
+    "url": "/Confidenceiskey/robofriends/index.html"
+  }
+];
