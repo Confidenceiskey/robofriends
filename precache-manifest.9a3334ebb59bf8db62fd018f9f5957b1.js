d2387610a7d13e21d6353f399b0a79e2f5db494a
David N <bigair_1080@hotmail.com>
2019-02-14 13:34:36 -0400

    Updates

---
+++ precache-manifest.9a3334ebb59bf8db62fd018f9f5957b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "c72ea0a72e3c194fe01c",
+    "url": "/robofriends/static/js/main.c72ea0a7.chunk.js"
+  },
+  {
+    "revision": "2b3b62a209a3477ff0df",
+    "url": "/robofriends/static/js/1.2b3b62a2.chunk.js"
+  },
+  {
+    "revision": "c72ea0a72e3c194fe01c",
+    "url": "/robofriends/static/css/main.e6871b24.chunk.css"
+  },
+  {
+    "revision": "2b3b62a209a3477ff0df",
+    "url": "/robofriends/static/css/1.47659e52.chunk.css"
+  },
+  {
+    "revision": "fcce249c0f5b3ddda260f639681005e3",
+    "url": "/robofriends/index.html"
+  }
+];
